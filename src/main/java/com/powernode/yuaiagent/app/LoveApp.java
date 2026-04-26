aff6f0d2a7e0d1d87b9fab437881d448690e8b96
runoob <test@runoob.com>
2026-04-26 20:31:57 +0800

    feat: add file-based chat memory and love report test

--- src/main/java/com/powernode/yuaiagent/app/LoveApp.java
+++ src/main/java/com/powernode/yuaiagent/app/LoveApp.java
@@ -1,5 +1,6 @@
 package com.powernode.yuaiagent.app;
 
+import com.powernode.yuaiagent.ChatMemory.FileBasedChatMemory;
 import com.powernode.yuaiagent.advisor.MyLoggerAdvisor;
 import com.powernode.yuaiagent.advisor.ReReadingAdvisor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,8 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
 
@@ -20,24 +23,27 @@ public class LoveApp {
 
     private final ChatClient chatClient;
 
+    record LoveReport(String title, List<String> suggestions) {
+    }
+
+
     private static final String SYSTEM_PROMPT = "扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。" +
             "围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；" +
             "恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。" +
             "引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。";
 
     public LoveApp(ChatModel dashscopeChatModel) {
-        // 初始化基于内存的对话记忆
-        ChatMemory chatMemory = new InMemoryChatMemory();
+        // 初始化基于文件的对话记忆
+        String fileDir = System.getProperty("user.dir") + "/chat-memory";
+        ChatMemory chatMemory = new FileBasedChatMemory(fileDir);
         chatClient = ChatClient.builder(dashscopeChatModel)
                 .defaultSystem(SYSTEM_PROMPT)
                 .defaultAdvisors(
                         new MessageChatMemoryAdvisor(chatMemory)
-                        //自定义advisor可按需要开
-                        //         new MyLoggerAdvisor(),
-                        //         new ReReadingAdvisor()
                 )
                 .build();
     }
+
 
     public String doChat(String message, String chatId) {
         ChatResponse response = chatClient
@@ -51,5 +57,19 @@ public class LoveApp {
         log.info("content: {}", content);
         return content;
     }
+
+    public LoveReport doChatWithReport(String message, String chatId) {
+        LoveReport loveReport = chatClient
+                .prompt()
+                .system(SYSTEM_PROMPT + "每次对话后都要生成恋爱结果，标题为{用户名}的恋爱报告，内容为建议列表")
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .call()
+                .entity(LoveReport.class);
+        log.info("loveReport: {}", loveReport);
+        return loveReport;
+    }
+
 
 }
